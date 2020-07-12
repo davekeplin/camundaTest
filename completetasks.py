3a0d50fde13c95b7a005c52889379ec09db8d4c4
Dave Keplin <davekeplin7@gmail.com>
2020-07-12 19:01:37 +0200

    replace by pycamunda.

--- completetasks.py
+++ completetasks.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pycamunda.task
+import pycamunda
 
 URL = "http://localhost:8080/engine-rest/"
 TASK = "findPiece"
@@ -8,24 +10,26 @@ def complete_some_task(key):
 	print("Started")
 	tasks = []
 	try: 
-		response = requests.get(URL +"task/", params={"taskDefinitionKey": key})
-	except requests.exceptions.RequestException:
+		tasks = pycamunda.task.GetList(url=URL, task_definition_key=key)()
+	except pycamunda.PyCamundaException:
 		pass
 	for t in json.loads(response.text):
 		tasks.append(t)
 
 	for i, t in enumerate(tasks):
-		if t["taskDefinitionKey"] != key:
+		if t.task_definition_key != key:
 			del tasks[i]
 	
 	for task in tasks:
 		try:
-			requests.post(URL+"task/{}/complete".format(task["id"]))
-		except requests.exceptions.RequestException:
+			pycamunda.task.Complete(url=URL, id_=task.id_)()
+		except pycamunda.PyCamundaException:
 			pass
 
 	print("Completed tasks.")
 
-	
-complete_some_task(TASK)
+
+if __name__ == "__main__":
+
+	complete_some_task(TASK)
 
